0a35e1b24612561a72a042a8fc749ad262a20d5f
Luis Mayorga <luismayorgam@gmail.com>
2014-03-11 17:49:11 +0100

    Initial commit, plugin actions

---
+++ src/be/ac/ua/main/DoHandler.java
@@ -0,0 +1,16 @@
+package be.ac.ua.main;
+
+import org.eclipse.core.commands.AbstractHandler;
+import org.eclipse.core.commands.ExecutionEvent;
+import org.eclipse.core.commands.ExecutionException;
+
+public class DoHandler extends AbstractHandler {
+
+	@Override
+	public Object execute(ExecutionEvent event) throws ExecutionException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+
+}
