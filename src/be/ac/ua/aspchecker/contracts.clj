c0eca0a5ded84dd8bfc254c1149c264f0a740234
Luis Mayorga <luismayorgam@gmail.com>
2014-03-26 17:23:45 +0100

    more normalization

---
+++ src/be/ac/ua/aspchecker/contracts.clj
@@ -0,0 +1,5 @@
+(ns 
+  ^{:doc "Contract processing."
+    :author "Luis Mayorga"}
+  be.ac.ua.aspchecker.contracts
+  (:require [be.ac.ua.aspchecker.model :as mod] ))
--- src/be/ac/ua/aspchecker/model.clj
+++ src/be/ac/ua/aspchecker/model.clj
@@ -3,7 +3,7 @@
     :author "Luis Mayorga"}
 be.ac.ua.aspchecker.model  
   (:require [damp.ekeko :as ek])
-  (:require [damp.ekeko.aspectj.weaverworld :as eaj])
+  (:require [damp.ekeko.aspectj.weaverworld :as wea])
   (:require [damp.ekeko.jdt [astnode :as ast]])
   (:require [damp.ekeko.jdt.convenience :as conv])
   (:require [damp.ekeko.jdt.astbindings :as astb])
@@ -15,7 +15,7 @@ be.ac.ua.aspchecker.model
   "All advices and their shadows"
   []
   (ek/ekeko [?advice ?shadow]
-            (eaj/advice-shadow 
+            (wea/advice-shadow 
               ?advice 
               ?shadow)))
 
@@ -24,7 +24,7 @@ be.ac.ua.aspchecker.model
   "All advices"
   []
   (ek/ekeko [?advice]
-            (eaj/advice 
+            (wea/advice 
               ?advice)))
 
 
@@ -32,7 +32,7 @@ be.ac.ua.aspchecker.model
   "All shadows"
   []
   (ek/ekeko [?shadow]
-            (eaj/shadow 
+            (wea/shadow 
               ?shadow)))
 
 
@@ -80,7 +80,7 @@ be.ac.ua.aspchecker.model
               ?annotation 
               "be.ac.ua.aspchecker.annotations.invariant")))
 
-;TODO maybe descompose in a simpler function and apply
+
 (defn binded|annotations
   "Annotation bindings"
   []
@@ -88,18 +88,6 @@ be.ac.ua.aspchecker.model
     (if (seq anns)
       (let [[_ abin] (first anns)]
         (recur(cons abin result) (rest anns)))
-      result)))
-
-
-;TODO maybe descompose in a simpler function and apply
-(defn advice-annotation
-  "Return pairs < BcelAdvice, AnnotationAJ vector > with all 
-the annotations corresponding to an advice"
-  []
-  (loop[result [], raw (advice-shadow)]
-    (if (seq raw)
-      (let [[adv _] (first raw)]
-        (recur (cons [adv (into [] (.getAnnotations (.getSignature adv)))] result) (rest raw)))
       result)))
 
 
@@ -123,9 +111,33 @@ and their value as stored value"
   [annotations]
   (loop [result {}, ans annotations]
     (if (seq ans)
-      (let[a (first ans)]
+      (let [a (first ans)]
         (if  (nil? (annotationtype a))
           (recur result (rest ans))
           (recur (assoc result (annotationtype a) (.getStringFormOfValue a "value")) 
                  (rest ans))))
       result)))
+
+
+(defn advice-annotation
+  "Return pairs < BcelAdvice, AnnotationAJ vector > with all 
+the annotations corresponding to an advice"
+  []
+  (loop[result [], raw (advices)]
+    (if (seq raw)
+      (let [[adv] (first raw)]
+        (recur (cons [adv (into [] (.getAnnotations (.getSignature adv)))] result) (rest raw)))
+      result)))
+
+
+
+(defn advice-annotation+
+  "Return pairs of BcelAdvice and a map containing the annotations. Differs
+to the other version in normalization of the annotations"
+  []
+  (let [pairs (advice-annotation)]
+    (map 
+      (fn [pair] 
+        (let [[a b] pair] 
+          (vector a (annotationaj-to-normalized b)))) 
+      pairs)))
