c42abea24d2a5af70e662a82ed1590bd96a79ae2
Luis Mayorga <luismayorgam@gmail.com>
2014-04-01 12:55:00 +0200

    contracts - java transformation separation

--- src/be/ac/ua/aspchecker/contracts.clj
+++ src/be/ac/ua/aspchecker/contracts.clj
@@ -3,65 +3,22 @@
     :author "Luis Mayorga"}
 be.ac.ua.aspchecker.contracts
   (:use [be.ac.ua.aspchecker.model])
-  (:import [be.ac.ua.aspchecker.utils Z3]))
+  (:use [be.ac.ua.aspchecker.z3]))
 
 
-(defn to-contract-pair
-  [{:keys [adv acon mtd mcon]}]
-  (case (advicetype adv)
-    :before (vector (vector (:ensures acon) (:requires mcon))) 
-    :after (vector (vector (:ensures mcon) (:requires acon)))
-    :around (vector (vector (:requires acon) (:requires mcon))
-                    (vector (:ensures mcon) (:ensures acon)))))
 
-
-(defn pair-contracts
-  []
-  (reduce conj [] (map 
-                    to-contract-pair 
-                    (advice-method-annotation))))
-
-
-;TODO
-(defn to-predicates
-  "Translate the expression into logic predicates."
-  [^String source]
-  source)
-
-
-;TODO
-(defn sorts
-  "Returns a vector containing pairs of symbols and their sorts."
-  [^String contract]
-  ([]))
-
-
-(defn compose-formulas
-  "Joins two formulas and add their metadata"
-  [form1 form2]
-  ([(str 
-      "(benchmark tst :formula (and not (" 
-      form1
-      ")("
-      form2
-      ")")
-    (cons (sorts form1) (sorts form2))]))
-
-
-(defn z3-evaluate
-  "Evaluates an SMTLIB string in Z3"
-  [str symbols]
-  (Z3/eval str symbols))
-
-
-(defn is-stronger-than
-  "Compares if the first contract is more restrictive than a second one."
+(defn is-stronger-than ^boolean
+"Compares if the first contract is more restrictive than a second one."
   [^String source ^String target]
-  (z3-evaluate (apply compose-formulas (map to-predicates [source target]))))
+  (sat-compare source target))
 
 
-;TODO
-(defn check-advice
-  [advice-info method-info]
-  "Checks that constraints are met between the corresponding contracts in the given elements."
-  ())
+(defn annotation-check
+  ([]
+    (map annotation-check (advice-method-annotation)))
+  ([{:keys [adv acon mtd mcon]}]
+    (case (advicetype adv)
+      :before (is-stronger-than (:ensures acon) (:requires mcon)) 
+      :after (is-stronger-than (:ensures mcon) (:requires acon))
+      :around (and (is-stronger-than (:requires acon) (:requires mcon))
+                   (is-stronger-than (:ensures mcon) (:ensures acon))))))
---
+++ src/be/ac/ua/aspchecker/z3.clj
@@ -0,0 +1,34 @@
+(ns be.ac.ua.aspchecker.z3
+    ^{:doc "Predicate transformation and z3 interop."
+    :author "Luis Mayorga"}
+  (:import [be.ac.ua.aspchecker.utils Z3]))
+
+
+
+(defn to-predicates
+  "Translate the expression into logic predicates."
+  [^String source]
+  source)
+
+
+(defn sorts
+  "Returns a vector containing pairs of symbols and their sorts."
+  [^String contract]
+  ([]))
+
+
+(defn compose-formulas
+  "Joins two formulas and add their metadata"
+  [form1 form2]
+  ([(str 
+      "(benchmark tst :formula (and not (" 
+      form1
+      ")("
+      form2
+      ")")
+    (cons (sorts form1) (sorts form2))]))
+
+
+(defn sat-compare
+  [source target]
+  ())
