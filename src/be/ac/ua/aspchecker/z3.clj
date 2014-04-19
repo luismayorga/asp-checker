9e05c4b1a0374e50c18bf8c4af3f04afe1fcc220
Luis Mayorga <luismayorgam@gmail.com>
2014-04-19 22:06:04 +0200

    z3 interop in clojure

--- src/be/ac/ua/aspchecker/z3.clj
+++ src/be/ac/ua/aspchecker/z3.clj
@@ -3,10 +3,30 @@
     :author "Luis Mayorga"}
 be.ac.ua.aspchecker.z3
   (:use be.ac.ua.aspchecker.transformation)
-  (:import [be.ac.ua.aspchecker.utils Z3]))
+  (:import [com.microsoft.z3 Context Status]))
+
+
+(defn init-solver []
+  (let [cfg (new java.util.HashMap)]
+    (do
+      (.put cfg "model" "true")
+      (new Context cfg))))
+
+
+(defn eval [ctx]
+  (let
+    [exp (nth (.SMTLIBFormulas ctx) 0)
+     solv (.MkSolver ctx)]
+    (do
+      (.Assert solv exp)
+      (= (.Check solv) Status/SATISFIABLE))))
+
+
+(defn clean-solver [ctx]
+  (.Dispose ctx))
 
 
 (defn more-restrictive? [source target]
- (let [cx (Z3/init)]
-  (.ParseSMTLIBString cx (str source target) nil nil nil nil)
-  (Z3/eval cx)))
+  (let [cx (init-solver)]
+    (.ParseSMTLIBString cx (str source target) nil nil nil nil)
+    (eval cx)))
